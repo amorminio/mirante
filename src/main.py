a5aa4cf0d4ca4abda80c234b5075a03a6431727d
Raphael Amorminio <raphael.amorminio@gmail.com>
2026-04-26 16:02:37 -0300

    Criação da tratativa do formato do arquivo de entrada. Instalação dos modulos argparse e charset-normalizer

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 import os
 from src.processing import parse_vendas
 from src.analytics import gerar_relatorio_faturamento
+from charset_normalizer import from_path
+
 
 def main():
-    
-    caminho = "data/exemplo1.csv"
+   
     #caminho = input("Digite o caminho do arquivo CSV: ")
+    caminho = "data/exemplo1.csv"
+    codificacao = detectar_encoding(caminho)
+    print(f"Codificação detectada: {codificacao}")
     
     try:
         print("Iniciando processamento...")
-        with open(caminho, mode="r", encoding="utf-8",errors="replace") as arquivo:
+        with open(caminho, mode="r", encoding=codificacao) as arquivo:
             print("Lendo arquivo...")
             vendas = parse_vendas(arquivo)
             print("Processando vendas...")
@@ -20,6 +24,13 @@ def main():
         print("Arquivo não encontrado!")
     except Exception as e:
         print(f"Erro ao processar arquivo: {e}")
+
+def detectar_encoding(caminho_arquivo:str) -> str:
+    resultado = from_path(caminho_arquivo).best()
+    if resultado:
+        return resultado.encoding
+    return "utf-8"
+
 
 if __name__ == "__main__":
     main()
